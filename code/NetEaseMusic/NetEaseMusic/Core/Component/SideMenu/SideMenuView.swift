2f82a03969b478b3a4d7f6e13efc1f3bb96db005
Hi, there! <arthurwanggang@yeah.net>
2022-12-30 22:58:04 +0800

    feat: start to develop sideMenu

---
+++ code/NetEaseMusic/NetEaseMusic/Core/Component/SideMenu/SideMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuView.swift
+//  NetEaseMusic
+//
+//  Created by Ray on 2022/12/30.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text("side menu")
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
---
+++ code/NetEaseMusic/NetEaseMusic/Core/Component/SideMenu/SideMenuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SideMenuViewModel.swift
+//  NetEaseMusic
+//
+//  Created by Ray on 2022/12/30.
+//
+
+import Foundation
