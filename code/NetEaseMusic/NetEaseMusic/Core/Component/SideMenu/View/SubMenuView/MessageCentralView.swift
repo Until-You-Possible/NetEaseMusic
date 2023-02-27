51b3702a6a2f7c0b15dc82f90f3be87c27562664
Hi, there! <arthurwanggang@yeah.net>
2023-02-27 18:09:18 +0800

    feat: keep doing the sideMenu

--- code/NetEaseMusic/NetEaseMusic/Core/Component/SideMenu/View/SubMenuView/MessageCentralView.swift
+++ code/NetEaseMusic/NetEaseMusic/Core/Component/SideMenu/View/SubMenuView/MessageCentralView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct MessageCentralView: View {
     var body: some View {
-        Text("message")
+        VStack {
+            HStack {
+                Image(systemName: "ellipsis.message")
+                Text("消息中心")
+                Spacer()
+                Image(systemName: "arrow.right")
+            }
+            .frame(height: 40, alignment: .center)
+            .padding([.leading, .trailing], 20)
+            Divider()
+                .padding([.leading, .trailing], 20)
+            HStack {
+                Image(systemName: "ellipsis.message")
+                Text("云贝中心")
+                Spacer()
+                Image(systemName: "arrow.right")
+            }
+            .frame(height: 40)
+            .padding([.leading, .trailing], 20)
+            Divider()
+                .padding([.leading, .trailing], 20)
+            HStack {
+                Image(systemName: "ellipsis.message")
+                Text("创作者中心")
+                Spacer()
+                Image(systemName: "arrow.right")
+            }
+            .frame(height: 40)
+            .padding([.leading, .trailing], 20)
+        }
+        .background(Color.white)
+        .cornerRadius(12)
+        .padding(.top, 10)
+
+        
+
     }
 }
 
