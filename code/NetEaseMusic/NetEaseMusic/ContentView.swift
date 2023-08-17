44aa92338cfa6a1759ee41b43637b1b9a8763b3b
ray <arthurwanggang@yeah.net>
2023-08-17 12:00:55 +0800

    refactor: 优化首页的代码

--- code/NetEaseMusic/NetEaseMusic/ContentView.swift
+++ code/NetEaseMusic/NetEaseMusic/ContentView.swift
@@ -5,80 +5,17 @@
 //  Created by Ray on 2022/12/4.
 //
 
-import SwiftUI
-import SwiftUIFontIcon
+import SwiftUI 
 
 struct ContentView: View {
     
     // the sidebar should be displayed here
     
     @State private var isLogin = true
-    @State private var isShowSideMenu = false
     
     var body: some View {
-        
         Group {
-            
-            ZStack(alignment: .topLeading) {
-                if isLogin {
-                     MainTabView()
-                } else {
-                    LoginView()
-                }
-                
-                //MARK: shadow for the sideMenu
-                if isShowSideMenu {
-                    ZStack {
-                        Color(.black)
-                            .animation(.easeInOut(duration: 0.5))
-                            .opacity(isShowSideMenu ? 0.25 : 0.0)
-                    }
-                    .onTapGesture {
-                        withAnimation(.easeInOut) {
-                            self.isShowSideMenu = false
-                        }
-                    }
-                    .ignoresSafeArea()
-                    
-                }
-                
-                //MARK: to show the sideMenu by button
-                SideMenuView()
-                    .frame(width: 300)
-                    .background(isShowSideMenu ? Color.white : Color.clear)
-                    .offset(x: isShowSideMenu ? 0: -300, y: 0)
-                
-//                if isShowSideMenu == false && isLogin {
-//                    Button {
-//                        // to show the sidebar
-//                        withAnimation(.easeInOut) {
-//                            self.isShowSideMenu = true
-//                        }
-//                    } label: {
-//                        Image(systemName: "sidebar.left")
-//                            .foregroundColor(.black)
-//                            .font(.system(size: 24))
-//
-//                    }
-//                    .padding(.leading, 16)
-//                }
-            }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarLeading) {
-//                    Button {
-//                        // to show the sidebar
-//                        withAnimation(.easeInOut) {
-//                            self.isShowSideMenu = true
-//                        }
-//                    } label: {
-//                        Image(systemName: "sidebar.left")
-//                            .foregroundColor(.black)
-//                            .font(.system(size: 24))
-//
-//                    }
-//                }
-//            }
-//            .ignoresSafeArea()
+            isLogin ? AnyView(MainTabView()) : AnyView(LoginView())
         }
     }
 }
