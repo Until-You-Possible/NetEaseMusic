cdc21a18c2cb49e8eb120f30347d00292d4f0c0b
ray <arthurwanggang@yeah.net>
2023-08-29 14:32:14 +0800

    feat: 开始开发播客页面

--- code/NetEaseMusic/NetEaseMusic/Core/views/Bodcast/BodCastView.swift
+++ code/NetEaseMusic/NetEaseMusic/Core/views/Bodcast/BodCastView.swift
@@ -9,6 +9,20 @@ import SwiftUI
 import SwiftUIFontIcon
 
 
+struct CategoryEntry {
+    var name: String
+}
+
+let CategoryEngtryList = [
+    CategoryEntry(name: "我的播客"),
+    CategoryEntry(name: "全部分类"),
+    CategoryEntry(name: "排行榜"),
+    CategoryEntry(name: "宝藏播客"),
+    CategoryEntry(name: "助眠解压"),
+    CategoryEntry(name: "广播电台")
+]
+
+
 struct BodCastView: View {
     
     @ObservedObject var viewModel = EntrySlideViewModel()
@@ -20,28 +34,55 @@ struct BodCastView: View {
         VStack () {
             
             //MARK: search areas
-            HStack () {
-                Text("搜索区域")
+            
+            NavigationLink {
+                SearchCenterView()
+            } label: {
+                HStack () {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                        .padding(.leading, 10)
+                    Text("搜索区域")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .frame(width: 360, height: 40)
+                .background(Color(hex: "#f2f3f6"))
+                .cornerRadius(40)
             }
             
             // MARK: some entry list
-            HStack () {
-                Text("部分入口区域")
+            ScrollView (.horizontal) {
+                HStack () {
+                    ForEach(CategoryEngtryList, id: \.name) { list in
+                        HStack () {
+                            /*@START_MENU_TOKEN@*/Text(list.name)/*@END_MENU_TOKEN@*/
+                        }
+                        .padding(10)
+                        .cornerRadius(6)
+                        .border(Color(.systemGray5), width: 1)
+                    }
+                }
             }
+            .padding(.leading)
+            .padding(.top, 20)
+            .padding(.bottom, 20)
+            
             
             // MARK: radio area
             VStack () {
                 Text("电台区域(难点 重点")
             }
+            .frame(width: UIScreen.main.bounds.width, height: 200)
+            .border(.gray, width: 1)
             
             // MARK: some recommendations list
             VStack () {
                 Text("部分推荐内容，可以复用首页的推荐组建")
             }
             
-            
+            Spacer()
         }
-        .background(.gray)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
--- code/NetEaseMusic/NetEaseMusic/Core/views/TestDemo/Test01.swift
+++ code/NetEaseMusic/NetEaseMusic/Core/views/TestDemo/Test01.swift
@@ -8,21 +8,44 @@
 import SwiftUI
 
 struct Test01: View {
+    
+    @State private var rotation: Double = 0.0
+    @State private var dragOffset: CGSize = .zero
+    @State private var isDragging = false
+
     var body: some View {
-        NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .toolbar {
-                    ToolbarItem () {
-                        Image(systemName: "plus")
-                            .background(.red)
-                            .foregroundColor(.white)
-                            .cornerRadius(20)
-                        
-                    }
-                }
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                let angle = atan2(value.location.y - 150, value.location.x - 150)
+                let rotation = angle * 180 / .pi
+                let damping: Double = 0.5 // Adjust the damping factor
+                let normalizedRotation = self.rotation + rotation
+                self.rotation = self.isDragging ? normalizedRotation * damping : normalizedRotation
+                self.isDragging = true
+            }
+            .onEnded { _ in
+                self.isDragging = false
+                // Add code for haptic feedback here
+            }
+
+        return VStack {
+            Text("Dial Value: \(Int(rotation))")
+                .padding()
+            ZStack {
+                Circle()
+                    .stroke(Color.gray, lineWidth: 20)
+                    .frame(width: 300, height: 300)
+                Circle()
+                    .trim(from: 0, to: 0.5)
+                    .stroke(Color.blue, lineWidth: 20)
+                    .frame(width: 300, height: 300)
+                    .rotationEffect(.degrees(rotation), anchor: .center)
+            }
+            .gesture(dragGesture)
         }
     }
 }
+
 
 struct Test01_Previews: PreviewProvider {
     static var previews: some View {
